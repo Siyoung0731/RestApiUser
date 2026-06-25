67126bbbc8eeb7971d6fd03bb46e3891cca3aa07
choisiyoung02 <azxx0731@naver.com>
2026-06-25 16:26:01 +0900

    first commit

---
+++ src/main/java/com/example/restapiuser/RestApiUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapiuser.restapiuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiUserApplication.class, args);
+    }
+
+}
